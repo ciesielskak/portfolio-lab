e1bbf7339039f3dbe81b8fe64a13a9cee4e377b3
ciesielskak <747karolina747@gmail.com>
2021-03-29 00:52:22 +0200

    form validation code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Decoration from '../assets/Decoration.svg';
 import Facebook from '../assets/Facebook.png';
 import Instagram from '../assets/Instagram.png'
 
+
+
 export const Contact = () => {
+    const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+});
+
+    const [error, setError] = useState({
+    nameError: '',
+    emailError: '',
+    messageError: ''
+})
+
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setForm(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+    const validateForm = () => {
+      let nameError = '';
+      let emailError = '';
+      let messageError = '';
+
+      if (form.name.includes(" ")) {
+          nameError = 'Podane imię jest niepoprawne!'
+      }
+      if (!form.email.includes('@')) {
+          emailError = 'Podany e-mail jest niepoprwany!'
+      }
+      if (form.message.length < 120) {
+          messageError = 'Wiadomość musi mieć conajmniej 120 znaków!'
+      }
+      if (nameError || emailError || messageError) {
+          setError(prevState => ({
+              ...prevState,
+                 nameError: nameError,
+                emailError: emailError,
+                messageError: messageError
+          }))
+          return false
+      }
+      return true
+    }
+
+   const sendForm = (e) => {
+        e.preventDefault();
+       const API = "http://localhost:3000";
+
+       const data = {
+           name: "Ania",
+           brand: "FSO"
+       };
+       const isValid = validateForm();
+       if (isValid) {
+           fetch(`${API}/cars`, {
+               method: "POST",
+               body: JSON.stringify(data),
+               headers: {
+                   "Content-Type": "application/json"
+               }
+           })
+               .then(response => response.json())
+               .then(data => {
+                   console.log(data);
+               })
+               .catch(error => {
+                   console.log(error);
+               });
+       }
+    }
     return (
         <div className='contact__container' id='contact'>
-            <form className='contact__form'>
+            <form onSubmit={sendForm} className='contact__form'>
                 <div className='contact__form__header'>
                     <h1>Skontaktuj się z nami
                         </h1>
@@ -15,16 +88,19 @@ export const Contact = () => {
                 <div className='contact__form__inputWrapper'>
                 <div className='contact__form__inputs'>
                     <h2>Wpisz swoje imię</h2>
-                    <input type='name'/>
+                    <h2>{error.nameError}</h2>
+                    <input type='name' name='name' value={form.name} onChange={inputHandler}/>
                 </div>
                 <div className='contact__form__inputs'>
                     <h2>Wpisz swój e-mail</h2>
-                    <input type='email'/>
+                    <h2 style={{color: 'red'}}>{error.emailError}</h2>
+                    <input type='email' name='email' value={form.email} onChange={inputHandler}/>
                 </div>
                 </div>
                 <div className='contact__form__textarea'>
                     <h2>Wpisz swoją wiadomość</h2>
-                    <textarea/>
+                    {error.messageError}
+                    <textarea name='message' value={form.message} onChange={inputHandler}/>
                 </div>
                 <button type='submit'>Wyślij</button>
 
