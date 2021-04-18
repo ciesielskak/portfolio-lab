d18ad43a0799287dabd5e2517d6d02d3c082fac2
ciesielskak <747karolina747@gmail.com>
2021-04-18 18:08:54 +0200

    rwd changes

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -10,23 +10,26 @@ export const HomeHeader = () => {
         setActive(prevState => !prevState)
     }
     return (
-        <>
-        <nav className={active? 'home__navigation active' : 'home__navigation'}>
-            <NavRouteLinks toggle={toggleNav}/>
+        <header className='home__navigation'>
+            <nav className={active? 'home__navigation__nav active' : 'home__navigation__nav'}>
+                    <NavRouteLinks toggle={toggleNav}/>
                     <ul className='home__navigation__links'>
-                <NavLink to='/' onClick={toggleNav}>Start</NavLink>
-                <Link to='foursteps' smooth={true} duration={1000} onClick={toggleNav}>O co chodzi?</Link>
-                <Link to='about' smooth={true} duration={1000} onClick={toggleNav}>O nas</Link>
-                <Link to='foundations' smooth={true} duration={1000} onClick={toggleNav}>Fundacja i organizacje</Link>
-                <Link to='contact' smooth={true} duration={1000} onClick={toggleNav}>Kontakt</Link>
+                        <NavLink to='/' onClick={toggleNav}>Start</NavLink>
+                        <Link to='foursteps' smooth={true} duration={1000} onClick={toggleNav}>O co chodzi?</Link>
+                        <Link to='about' smooth={true} duration={1000} onClick={toggleNav}>O nas</Link>
+                        <Link to='foundations' smooth={true} duration={1000} onClick={toggleNav}>Fundacja i organizacje</Link>
+                        <Link to='contact' smooth={true} duration={1000} onClick={toggleNav}>Kontakt</Link>
                     </ul>
-        </nav>
-        <div className='home__navigation__burger' onClick={toggleNav}>
-        <span className='home__navigation__burger__1'></span>
-        <span className='home__navigation__burger__2'></span>
-        <span className='home__navigation__burger__3'></span>
-    </div>
-    </>
+            </nav>
+            <div className='home__navigation__burger' onClick={toggleNav}>
+                <div className='home__navigation__burger__wrapper'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </header>
+
     )
 
 }
